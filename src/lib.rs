939a9fbac37d2f341a50eb343d2cae32391a47f7
not 0x12 <kalanamith@outlook.com>
2024-10-09 19:45:45 +1300

    Ported code (#1)
    * Ported code
    
    * Ported code
    
    * Ported code
    
    * Ported code

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(form: web::Form<FromData>) -> impl Responder {
+    let _x = form.email.clone();
+    let _y = form.name.clone();
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct FromData {
+    email: String,
+    name: String,
+}
+
+
+pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    }).listen(listener)?
+        .run();
+    Ok(server)
+}
